b1ca631c45c29967c36cf90c9c92161594830163
Piero del Valle <pdelvallebaudac8221@conestogac.on.ca>
2025-09-25 16:26:11 -0400

    change privacy page to about

--- MoviesApp/Controllers/HomeController.cs
+++ MoviesApp/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace MoviesApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
